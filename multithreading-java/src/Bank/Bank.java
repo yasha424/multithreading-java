ab717fc6f897b9d4846bb62a76074c2f399597d2
yasha424 <jakob20022404@gmail.com>
2023-05-07 21:18:39 +0300

    Add task 1

---
+++ multithreading-java/src/Bank/Bank.java
@@ -0,0 +1,102 @@
+package Bank;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Bank {
+    public static final int NTEST = 10000;
+    private final int[] accounts;
+    private long ntransacts = 0;
+    private final ReentrantLock locker = new ReentrantLock();
+    Condition emptyCondition = locker.newCondition();
+
+    public Bank(int n, int initialBalance){
+        accounts = new int[n];
+        int i;
+        for (i = 0; i < accounts.length; i++)
+            accounts[i] = initialBalance;
+        ntransacts = 0;
+    }
+
+    public synchronized void synchronizedTransfer(int from, int to, int amount) {
+       while (accounts[from] < amount) {
+           try {
+               wait();
+           } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+           }
+       }
+
+        accounts[from] -= amount;
+        accounts[to] += amount;
+        ntransacts++;
+        if (ntransacts % NTEST == 0) {
+            test();
+        }
+
+        notifyAll();
+    }
+
+    public void synchronizedBlockTransfer(int from, int to, int amount) {
+        synchronized (this) {
+            while (accounts[from] < amount) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            accounts[from] -= amount;
+            accounts[to] += amount;
+            ntransacts++;
+            if (ntransacts % NTEST == 0) {
+                test();
+            }
+
+            notifyAll();
+        }
+    }
+
+    public void lockedTransfer(int from, int to, int amount) {
+        locker.lock();
+        try {
+            while (accounts[from] < amount) {
+                try {
+                    emptyCondition.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            accounts[from] -= amount;
+            accounts[to] += amount;
+            ntransacts++;
+            if (ntransacts % NTEST == 0) {
+                test();
+            }
+        } finally {
+            emptyCondition.signalAll();
+            locker.unlock();
+        }
+    }
+
+    public void transfer(int from, int to, int amount) {
+        accounts[from] -= amount;
+        accounts[to] += amount;
+        ntransacts++;
+        if (ntransacts % NTEST == 0)
+            test();
+    }
+
+    public void test(){
+        int sum = 0;
+        for (int i = 0; i < accounts.length; i++)
+            sum += accounts[i] ;
+        System.out.println("Transactions:" + ntransacts
+                + " Sum: " + sum);
+    }
+    public int size(){
+        return accounts.length;
+    }
+}
