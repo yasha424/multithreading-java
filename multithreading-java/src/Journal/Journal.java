2c96531c723d026cc0fce46fb20ba2693b33dabc
yasha424 <jakob20022404@gmail.com>
2023-05-25 12:56:31 +0300

    Also tried with lockers

--- multithreading-java/src/Journal/Journal.java
+++ multithreading-java/src/Journal/Journal.java
@@ -2,21 +2,25 @@ package Journal;
 
 import Journal.Groups.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Journal {
-    private final Map<Student, ArrayBlockingQueue<Integer>[]> grades = new HashMap<>();
+    private final Map<Student, ArrayList<Integer>[]> grades = new HashMap<>();
+    private final Map<Student, ArrayList<ReentrantLock>> lockers = new HashMap<>();
     private final int weeks;
 
     public Journal(Group[] groups, int weeks) {
         this.weeks = weeks;
         for (Group group : groups) {
             for (Student student : group.getStudents()) {
-                ArrayBlockingQueue<Integer>[] grades = new ArrayBlockingQueue[weeks];
+                lockers.put(student, new ArrayList<>());
+                ArrayList<Integer>[] grades = new ArrayList[weeks];
                 for (int i = 0; i < weeks; i++) {
-                    grades[i] = new ArrayBlockingQueue<>(4);
+                    grades[i] = new ArrayList<>(4);
+                    lockers.get(student).add(new ReentrantLock());
                 }
                 this.grades.put(student, grades);
             }
@@ -27,11 +31,16 @@ public class Journal {
         if (grade < 0 || grade > 100) {
             throw new RuntimeException("Invalid grade");
         }
-        grades.get(student)[week].add(grade);
+        try {
+            lockers.get(student).get(week).lock();
+            grades.get(student)[week].add(grade);
+        } finally {
+            lockers.get(student).get(week).unlock();
+        }
     }
 
     public void print() {
-        for (Map.Entry<Student, ArrayBlockingQueue<Integer>[]> entry : this.grades.entrySet()) {
+        for (Map.Entry<Student, ArrayList<Integer>[]> entry : this.grades.entrySet()) {
             System.out.println(entry.getKey().group().getName() + " " + entry.getKey().name());
             for (int i = 0; i < entry.getValue().length; i++) {
                 System.out.print("\tWeek №" + i + " ");
